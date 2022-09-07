62c448429f17c18fc5bd416d401625f6ef35f8ca
648540858 <648540858@qq.com>
2022-09-07 10:50:12 +0800

    添加支持rtc版本的zlm打包

--- README.md
+++ README.md
@@ -4,11 +4,14 @@
 
 linux下：
 
-```
+```shell
 # 安装环境
 ./install.sh centos7
-# 构建
-./build.sh centos7
+# 构建普通版本
+sudo ./build.sh centos7
+
+# 构建支持WEBRTC的版本
+sudo ./build.sh centos7 rtc
 
 # 移除安装环境
 ./uninstall.sh centos7
@@ -16,7 +19,7 @@ linux下：
 
 win下：
 
-```
+```shell
 # 安装环境
 ./install.bat centos7
 # 构建
@@ -26,7 +29,8 @@ win下：
 ./uninstall.bat centos7
 ```
 
-构建完成后可在build下找到构建成功的文件Release
+
+构建完成后可在build下找到构建成功的文件
 
 
 需要支持的操作系统，请大家在issues里留言。
--- build.sh
+++ build.sh
@@ -1,8 +1,26 @@
 #!/bin/bash  
-workdir=$(cd $(dirname $0); pwd) 
+workdir=$(cd $(dirname $0); pwd)
 
-docker run -it -v $workdir/build/:/home/ZLMediaKit zlm:$1 /home/ZLMediaKit/run.sh
+mkdir -p build
 
-currentTime=`date "+%Y%m%d"`
+currentTime=$(date "+%Y%m%d")
 
-mv build/Release build/zlm-release-$1-$currentTime
+realPath="zlm-release-$1-$currentTime"
+
+if [[ $2 == rtc ]]; then
+        realPath="zlm-release-rtc-$1-$currentTime"
+    fi
+
+cd build || exit
+rm -rf "$realPath"
+
+docker run -it -v "$workdir"/build/:/home/ZLMediaKit -v "$workdir"/script/:/home/script zlm:"$1" /home/script/run"$2".sh
+
+mv Release "$realPath"
+
+if [[ $2 == rtc ]]; then
+        cp ../script/installForRtc.sh "$reallPath"/install.sh
+        chmod +x "$reallPath"/install.sh
+    fi
+
+
